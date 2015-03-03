fde6338c41fa4992d53fc38c4b0909f6bbcee5c7
morrieati <morrislew@outlook.com>
2015-03-03 20:10:54 +0800

    Version 1.0
    Only Web Page Version.

---
+++ LJA_mine/script/main.js
@@ -0,0 +1,11 @@
+function Display (id)
+{
+	if (document.getElementById(id).style.display == 'none') 
+	{
+		document.getElementById(id).style.display = 'block';
+	}
+	else
+	{
+		document.getElementById(id).style.display = 'none';
+	}
+}
---
+++ script/demo.js
@@ -0,0 +1,7 @@
+var x = document.getElementById("button");
+x.addEventListener("click", demo1);
+e = "hello";
+function demo1 (e) 
+{
+	document.write(e);
+}
